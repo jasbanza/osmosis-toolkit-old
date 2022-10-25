61c215e34d59b403a27c9615c8bbf25731e39586
jasbanza <jason.banfield.za@gmail.com>
2022-10-25 05:03:08 +0200

    fetch gamms and do UI

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.btnCheckUnbondedGammAmounts = exports.btn_gammAmountHalf_onClick = exports.btn_gammAmountMax_onClick = exports.btn_bond_onClick = exports.select_gamms_onChange = exports.btnConnectKeplr_onClick = void 0;
+const long_1 = __importDefault(require("long"));
+// import { WalletManager } from '@cosmos-kit/core';
+const osmojs_1 = require("osmojs");
+// console.log(chain);
+// const wm = new WalletManager("")
+// import { createApp } from "vue";
+// import App from "./src/App.vue";
 (() => __awaiter(void 0, void 0, void 0, function* () {
     // waits for window.keplr to exist (if extension is installed, enabled and injecting it's content script)
     yield getKeplr();
@@ -39,10 +50,12 @@ function keplr_connectOsmosis() {
 function getOsmosisWallet() {
     var _a;
     return __awaiter(this, void 0, void 0, function* () {
+        ui_resetForm();
         const wallet = yield ((_a = window.keplr) === null || _a === void 0 ? void 0 : _a.getKey("osmosis-1").then((user_key) => {
             console.log(user_key);
             return user_key;
         }));
+        ui_showElementById("container_unbondedLPs");
         return wallet;
     });
 }
@@ -72,9 +85,11 @@ function getKeplr() {
 // EVENT HANDLERS
 function keplr_chains_onConnected() {
     return __awaiter(this, void 0, void 0, function* () {
+        ui_resetForm();
         const wallet = yield getOsmosisWallet();
         ui_setWallet(wallet);
         // update UI
+        ui_showElementById("container_unbondedLPs");
         // register event handler: if user changes account:
         window.addEventListener("keplr_keystorechange", keplr_keystore_onChange);
         //const offlineSigner = window.getOfflineSigner(chainId);
@@ -82,6 +97,7 @@ function keplr_chains_onConnected() {
 }
 function keplr_chains_onRejected() {
     return __awaiter(this, void 0, void 0, function* () {
+        ui_resetForm();
         ui_setWallet(undefined);
     });
 }
@@ -91,20 +107,145 @@ function keplr_keystore_onChange(e) {
         ui_setWallet(wallet);
     });
 }
-// utilized in inline html
+// EXPORTED TO A GLOBAL "module" OBJECT FOR INLINE HTML DOM EVENT LISTENERS
 function btnConnectKeplr_onClick() {
     return __awaiter(this, void 0, void 0, function* () {
         // connect Keplr wallet extension
         keplr_connectOsmosis();
-        // TODO: remove connect button, and show disconnect button
+        // TODO: rename connect button to reconnect button
     });
 }
+exports.btnConnectKeplr_onClick = btnConnectKeplr_onClick;
+let selectedGamm = { denom: "", amount: "0" };
+function select_gamms_onChange(el) {
+    //if selected is gamm, then show bonding durations
+    if (el.value == "") {
+        ui_hideElementById("container_bondingdurations");
+    }
+    else {
+        const option_selectedGamm = el.options[el.selectedIndex];
+        selectedGamm = {
+            denom: option_selectedGamm === null || option_selectedGamm === void 0 ? void 0 : option_selectedGamm.dataset.denom,
+            amount: option_selectedGamm === null || option_selectedGamm === void 0 ? void 0 : option_selectedGamm.dataset.amount,
+        };
+        ui_showElementById("container_bondingdurations");
+    }
+}
+exports.select_gamms_onChange = select_gamms_onChange;
+//dataset.amount
+function btn_bond_onClick(duration_days) {
+    console.log(selectedGamm);
+    console.log(duration_days);
+}
+exports.btn_bond_onClick = btn_bond_onClick;
+function btn_gammAmountMax_onClick() {
+    let input_gammAmountToBond = (document.getElementById("input_gammAmountToBond"));
+    if (selectedGamm.amount) {
+        const amount = long_1.default.fromString(selectedGamm.amount);
+        input_gammAmountToBond.value = Math.floor(amount.toNumber()).toString();
+    }
+}
+exports.btn_gammAmountMax_onClick = btn_gammAmountMax_onClick;
+function btn_gammAmountHalf_onClick() {
+    let input_gammAmountToBond = (document.getElementById("input_gammAmountToBond"));
+    if (selectedGamm.amount) {
+        const amount = long_1.default.fromString(selectedGamm.amount);
+        input_gammAmountToBond.value = Math.floor(amount.div(2).toNumber()).toString();
+    }
+}
+exports.btn_gammAmountHalf_onClick = btn_gammAmountHalf_onClick;
+function btnCheckUnbondedGammAmounts() {
+    return __awaiter(this, void 0, void 0, function* () {
+        ui_toggleMask("Loading unbonded LP balances...");
+        const { createRPCQueryClient } = osmojs_1.osmosis.ClientFactory;
+        const client = yield createRPCQueryClient({
+            rpcEndpoint: "https://rpc.osmosis.interbloc.org",
+        });
+        let address;
+        yield getOsmosisWallet().then((res) => {
+            address = res === null || res === void 0 ? void 0 : res.bech32Address;
+        });
+        if (address) {
+            const response = yield client.cosmos.bank.v1beta1.allBalances({
+                address: address,
+                pagination: {
+                    key: new Uint8Array(),
+                    offset: new long_1.default.fromString("0"),
+                    limit: new long_1.default.fromString("1000"),
+                    countTotal: true,
+                    reverse: false,
+                },
+            });
+            const arrCoins = response.balances;
+            const gamms = [];
+            for (const coin of arrCoins) {
+                if (coin.denom.indexOf("gamm") == 0) {
+                    gamms.push(coin);
+                }
+            }
+            ui_renderGamms(gamms);
+        }
+        ui_toggleMask();
+    });
+}
+exports.btnCheckUnbondedGammAmounts = btnCheckUnbondedGammAmounts;
 // UI FUNCTIONS
 function ui_setWallet(wallet) {
+    ui_resetForm();
     if (wallet) {
         document.querySelector("#wallet-status").innerHTML = `${wallet.bech32Address} - ${wallet.name}`;
+        ui_showElementById("container_unbondedLPs");
     }
     else {
         document.querySelector("#wallet-status").innerHTML = `WALLET NOT CONNECTED`;
     }
+}
+function ui_resetForm() {
+    ui_hideElementById("container_unbondedLPs"); //step 1
+    ui_hideElementById("container_select_gamms"); //step 2
+    // ui_hideElementById("select_gamms"); //step 2
+    ui_hideElementById("container_bondingdurations"); //step 3
+    ui_clearGamms();
+}
+function ui_renderGamms(gamms) {
+    ui_clearGamms();
+    const el_selectGamms = document.querySelector("#select_gamms");
+    if (gamms.length == 0) {
+        //todo set message
+        ui_showElementById("msg_gammsEmpty");
+        return;
+    }
+    const defaultOption = document.createElement("option");
+    defaultOption.value = "";
+    defaultOption.innerHTML = `2. Select an LP from this list`;
+    el_selectGamms === null || el_selectGamms === void 0 ? void 0 : el_selectGamms.append(defaultOption);
+    for (const coin of gamms) {
+        const option = document.createElement("option");
+        option.innerHTML = `<b>${coin.denom}</b> - ${coin.amount}`;
+        if (option instanceof HTMLOptionElement) {
+            option.value = coin.denom;
+            option.dataset.denom = coin.denom;
+            option.dataset.amount = coin.amount;
+        }
+        el_selectGamms === null || el_selectGamms === void 0 ? void 0 : el_selectGamms.append(option);
+    }
+    ui_hideElementById("msg_gammsEmpty");
+    ui_showElementById("container_select_gamms");
+}
+function ui_toggleMask(text = "Loading...") {
+    var _a;
+    (_a = document.querySelector("#mask")) === null || _a === void 0 ? void 0 : _a.classList.toggle("hidden");
+    document.querySelector("#mask div").innerHTML = text;
+}
+function ui_clearGamms() {
+    ui_hideElementById("container_select_gamms");
+    document.querySelector("#select_gamms").innerHTML = "";
+}
+function ui_showElementById(elId) {
+    var _a;
+    (_a = document.querySelector(`#${elId}`)) === null || _a === void 0 ? void 0 : _a.classList.remove("hidden");
+}
+function ui_hideElementById(elId) {
+    var _a;
+    (_a = document.querySelector(`#${elId}`)) === null || _a === void 0 ? void 0 : _a.classList.add("hidden");
 }
--- index.ts
+++ index.ts
@@ -1,4 +1,17 @@
+// import { chain, assets, asset_list, testnet, testnet_assets } from '@chain-registry/osmosis';
 import { Keplr } from "@keplr-wallet/types";
+import Long from "long";
+
+// import { WalletManager } from '@cosmos-kit/core';
+import { osmosis } from "osmojs";
+import { PageRequest } from "osmojs/types/codegen/cosmos/base/query/v1beta1/pagination";
+import { Coin } from "osmojs/types/codegen/cosmos/base/v1beta1/coin";
+// console.log(chain);
+
+// const wm = new WalletManager("")
+
+// import { createApp } from "vue";
+// import App from "./src/App.vue";
 
 (async () => {
   // waits for window.keplr to exist (if extension is installed, enabled and injecting it's content script)
@@ -41,10 +54,12 @@ async function getOsmosisWallet(): Promise<
     }
   | undefined
 > {
+  ui_resetForm();
   const wallet = await window.keplr?.getKey("osmosis-1").then((user_key) => {
     console.log(user_key);
     return user_key;
   });
+  ui_showElementById("container_unbondedLPs");
   return wallet;
 }
 
@@ -79,9 +94,11 @@ async function getKeplr(): Promise<Keplr | undefined> {
 
 // EVENT HANDLERS
 async function keplr_chains_onConnected(): Promise<void> {
+  ui_resetForm();
   const wallet = await getOsmosisWallet();
   ui_setWallet(wallet);
   // update UI
+  ui_showElementById("container_unbondedLPs");
 
   // register event handler: if user changes account:
   window.addEventListener("keplr_keystorechange", keplr_keystore_onChange);
@@ -90,6 +107,7 @@ async function keplr_chains_onConnected(): Promise<void> {
 }
 
 async function keplr_chains_onRejected(): Promise<void> {
+  ui_resetForm();
   ui_setWallet(undefined);
 }
 
@@ -98,11 +116,88 @@ async function keplr_keystore_onChange(e: any): Promise<void> {
   ui_setWallet(wallet);
 }
 
-// utilized in inline html
-async function btnConnectKeplr_onClick(): Promise<void> {
+// EXPORTED TO A GLOBAL "module" OBJECT FOR INLINE HTML DOM EVENT LISTENERS
+
+export async function btnConnectKeplr_onClick(): Promise<void> {
   // connect Keplr wallet extension
   keplr_connectOsmosis();
-  // TODO: remove connect button, and show disconnect button
+  // TODO: rename connect button to reconnect button
+}
+
+let selectedGamm: {
+  denom: string | undefined;
+  amount: string | undefined;
+} = { denom: "", amount: "0" };
+export function select_gamms_onChange(el: HTMLSelectElement) {
+  //if selected is gamm, then show bonding durations
+  if (el.value == "") {
+    ui_hideElementById("container_bondingdurations");
+  } else {
+    const option_selectedGamm = el.options[el.selectedIndex];
+    selectedGamm = {
+      denom: option_selectedGamm?.dataset.denom,
+      amount: option_selectedGamm?.dataset.amount,
+    };
+    ui_showElementById("container_bondingdurations");
+  }
+}
+
+//dataset.amount
+export function btn_bond_onClick(duration_days: number) {
+  console.log(selectedGamm);
+  console.log(duration_days);
+}
+
+export function btn_gammAmountMax_onClick() {
+  let input_gammAmountToBond = <HTMLInputElement>(
+    document.getElementById("input_gammAmountToBond")
+  );
+  if (selectedGamm.amount) {
+    const amount = Long.fromString(selectedGamm.amount);
+    input_gammAmountToBond.value = Math.floor(amount.toNumber()).toString();
+  }
+}
+
+export function btn_gammAmountHalf_onClick() {
+  let input_gammAmountToBond = <HTMLInputElement>(
+    document.getElementById("input_gammAmountToBond")
+  );
+  if (selectedGamm.amount) {
+    const amount = Long.fromString(selectedGamm.amount);
+    input_gammAmountToBond.value = Math.floor(amount.div(2).toNumber()).toString();
+  }}
+
+export async function btnCheckUnbondedGammAmounts(): Promise<void> {
+  ui_toggleMask("Loading unbonded LP balances...");
+  const { createRPCQueryClient } = osmosis.ClientFactory;
+  const client = await createRPCQueryClient({
+    rpcEndpoint: "https://rpc.osmosis.interbloc.org",
+  });
+  let address: string | undefined;
+  await getOsmosisWallet().then((res) => {
+    address = res?.bech32Address;
+  });
+  if (address) {
+    const response = await client.cosmos.bank.v1beta1.allBalances({
+      address: address,
+      pagination: {
+        key: new Uint8Array(),
+        offset: new (Long as any).fromString("0"),
+        limit: new (Long as any).fromString("1000"),
+        countTotal: true,
+        reverse: false,
+      },
+    });
+    const arrCoins = response.balances;
+    const gamms: Coin[] = [];
+    for (const coin of arrCoins) {
+      if (coin.denom.indexOf("gamm") == 0) {
+        gamms.push(coin);
+      }
+    }
+    ui_renderGamms(gamms);
+  }
+  ui_toggleMask();
 }
 
 // UI FUNCTIONS
@@ -117,13 +212,69 @@ function ui_setWallet(
       }
     | undefined
 ): void {
+  ui_resetForm();
   if (wallet) {
     document.querySelector(
       "#wallet-status"
     )!.innerHTML = `${wallet.bech32Address} - ${wallet.name}`;
+    ui_showElementById("container_unbondedLPs");
   } else {
     document.querySelector(
       "#wallet-status"
     )!.innerHTML = `WALLET NOT CONNECTED`;
   }
+}
+
+function ui_resetForm() {
+  ui_hideElementById("container_unbondedLPs"); //step 1
+  ui_hideElementById("container_select_gamms"); //step 2
+  // ui_hideElementById("select_gamms"); //step 2
+  ui_hideElementById("container_bondingdurations"); //step 3
+  ui_clearGamms();
+}
+
+function ui_renderGamms(gamms: Coin[]) {
+  ui_clearGamms();
+  const el_selectGamms = document.querySelector("#select_gamms");
+  if (gamms.length == 0) {
+    //todo set message
+
+    ui_showElementById("msg_gammsEmpty");
+    return;
+  }
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.innerHTML = `2. Select an LP from this list`;
+  el_selectGamms?.append(defaultOption);
+
+  for (const coin of gamms) {
+    const option = document.createElement("option");
+    option.innerHTML = `<b>${coin.denom}</b> - ${coin.amount}`;
+    if (option instanceof HTMLOptionElement) {
+      option.value = coin.denom;
+      option.dataset.denom = coin.denom;
+      option.dataset.amount = coin.amount;
+    }
+    el_selectGamms?.append(option);
+  }
+  ui_hideElementById("msg_gammsEmpty");
+
+  ui_showElementById("container_select_gamms");
+}
+
+function ui_toggleMask(text: string = "Loading...") {
+  document.querySelector("#mask")?.classList.toggle("hidden");
+  document.querySelector("#mask div")!.innerHTML = text;
+}
+
+function ui_clearGamms() {
+  ui_hideElementById("container_select_gamms");
+  document.querySelector("#select_gamms")!.innerHTML = "";
+}
+
+function ui_showElementById(elId: string) {
+  document.querySelector(`#${elId}`)?.classList.remove("hidden");
+}
+function ui_hideElementById(elId: string) {
+  document.querySelector(`#${elId}`)?.classList.add("hidden");
 }
